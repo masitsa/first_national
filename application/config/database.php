c180889b1a0e753f9bde0cd3298d174d7449e8cd
marttkip <marttkip@gmail.com>
2015-02-04 09:38:08 +0300

    nothing here

--- application/config/database.php
+++ application/config/database.php
@@ -50,7 +50,7 @@ $active_record = TRUE;
 
 $db['default']['hostname'] = 'localhost';
 $db['default']['username'] = 'root';
-$db['default']['password'] = 'jacinta123';
+$db['default']['password'] = '';
 $db['default']['database'] = 'first_national';
 $db['default']['dbdriver'] = 'mysql';
 $db['default']['dbprefix'] = '';
